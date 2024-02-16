7c6b9a6238a8abf607c2494241a62c4166ace1b7
willswats <willswats@users.noreply.github.com>
2024-02-16 12:15:42 +0000

    Add to frontend

--- frontend.py
+++ frontend.py
@@ -1,32 +1,60 @@
-from backend import CookingModes, SmartAirFryer, SmartHome, SmartPlug
+from enum import Enum
+from backend import SmartAirFryer, SmartHome, SmartPlug
 
 
-def check_valid_device_num(device_num):
-    valid_device_nums = ["1", "2"]
-    device_num_clean = device_num.lower().strip()
-    if device_num_clean in valid_device_nums:
+class SmartDeviceNums(Enum):
+    SMART_PLUG = "1"
+    SMART_AIR_FRYER = "2"
+
+
+def check_valid_device_num(smart_device_num):
+    valid_smart_device_nums = [
+        smart_device_num.value for smart_device_num in SmartDeviceNums
+    ]
+    smart_device_num_clean = smart_device_num.lower().strip()
+    if smart_device_num_clean in valid_smart_device_nums:
         return True
     return False
 
 
+def get_smart_device(smart_device_num):
+    match smart_device_num:
+        case SmartDeviceNums.SMART_PLUG.value:
+            while True:
+                try:
+                    consumption_rate = int(input("Enter consumption rate:"))
+                    smart_plug = SmartPlug(consumption_rate)
+                    print("Added Smart Plug.")
+                    return smart_plug
+                except ValueError as error:
+                    print("Error:", error)
+        case SmartDeviceNums.SMART_AIR_FRYER.value:
+            smart_air_fryer = SmartAirFryer()
+            print("Added Smart Air Fryer.")
+            return smart_air_fryer
+
+
 def get_smart_devices():
-    device_nums = []
-    print("Add 5 devices to your smart home:")
-    print("  1. Smart Plug")
-    print("  2. Smart Air Fryer")
-    while len(device_nums) < 5:
-        device_num = input("Enter device number: ")
-        if check_valid_device_num(device_num):
-            print("Added device.")
-            device_nums.append(device_num)
+    smart_devices = []
+    print("Add 5 smart devices to your smart home:")
+    print(f"  {SmartDeviceNums.SMART_PLUG.value}. Smart Plug")
+    print(f"  {SmartDeviceNums.SMART_AIR_FRYER.value}. Smart Air Fryer")
+    while len(smart_devices) < 5:
+        smart_device_num = input("Enter smart device number: ")
+        if check_valid_device_num(smart_device_num):
+            smart_device = get_smart_device(smart_device_num)
+            smart_devices.append(smart_device)
         else:
-            print("Invalid device number.")
-    return device_nums
+            print("Error: Invalid smart device number.")
+    return smart_devices
 
 
 def set_up_home():
     smart_home = SmartHome()
-    print(get_smart_devices())
+    smart_devices = get_smart_devices()
+    for smart_device in smart_devices:
+        smart_home.add_device(smart_device)
+    print(smart_home)
 
 
 class SmartHomeSystem:
